1ab834697af60d53da9f0af03f5422ebbf057a02
being-varun <varunsubramaniam16@gmail.com>
2021-10-04 15:18:09 +0530

    Simple login - Learning 02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React,{Component} from 'react';
-class App extends Component{
-    render(){
-        return(
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return (
             <div>
-            LogIn:<input type="text"></input>
-            Password:<input type="Password"></input>
+                <table>
+                    <tr>
+                        <td>
+                            Login nId:
+                        </td>
+                        <td>
+                            <input type="text"></input>
+                        </td>
+
+                    </tr>
+                    <tr>
+                        <td>
+                            Password:
+                        </td>
+                        <td>
+                            <input type="Password"></input>
+                        </td>
+                    </tr>
+                </table>
+
+                <br />
             </div>
         )
     }
